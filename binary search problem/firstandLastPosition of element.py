fa967fdddf73611958e193f370afa2ff213081f7
Sagar Saini <167205107+sagarsaini010@users.noreply.github.com>
2025-07-11 06:00:45 +0530

    Add files via upload

---
+++ binary search problem/firstandLastPosition of element.py
@@ -0,0 +1,24 @@
+def firstAndLastPosition(arr,k):
+    n = len(arr)
+    left =0
+    right =n-1
+    ans1=-1
+    ans2=n
+
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] <= k:
+            if arr[mid]==k:
+                ans1=mid
+            left = mid+1
+        elif arr[mid] >= k:
+            if arr[mid]==k:
+                ans2=mid
+            right = mid-1
+    return ans1,ans2
+
+
+nums = [5,7,7,8,8,10]
+
+ans = firstAndLastPosition(nums,8)
+print(ans)
