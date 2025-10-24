0adb1d5df864b0d085aca5f9f15031bf1910c26d
Sagar saini <sagarsaini954818@gmail.com>
2025-10-24 08:59:56 +0530

    for-max-sum

--- sliding window/max_sum.py
+++ sliding window/max_sum.py
@@ -1,25 +1,19 @@
-class Solution:
+class Solution():
     def max_sum(self,arr,k):
         n = len(arr)
-        i = 0
-        j = 0
-        sum = 0
+        start = 0 
+        sum = 0 
         max_sum = -10**9
-        while j < n:
-            sum = sum+arr[j]
-            if j < k-1:
-                j+=1
-            elif j-i+1 == k:
-                max_sum = max(sum,max_sum)
-                sum = sum - arr[i]
-                i+=1
-                j+=1
+        for i in range(n):
+            sum = sum + arr[i]
+            if i - start + 1 == k:
+                max_sum = max(max_sum,sum)
+                sum = sum - arr[start]
+                start+=1
         return max_sum
-                
-                
+        
+sol = Solution()
 arr = [1,2,3,4,5,6,7,8,9]
 
-sol = Solution()
-maxSum = sol.max_sum(arr,3)
-print(maxSum)
-            
+maxsum = sol.max_sum(arr,3)
+print(maxsum)
